b24138a6adc88d6c5a1062dd8f14a85d25517ab9
Cleiber Ziviani <cleiberz@yahoo.com.br>
2021-02-15 14:13:34 -0300

    configurações expressa carregadas

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ import bodyParser from 'body-parser'
 import morgan from 'morgan'
 import mongoose from 'mongoose'
 import cors from 'cors'
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
+app.use(cors())
